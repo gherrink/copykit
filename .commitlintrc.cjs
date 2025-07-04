2d7acbf703127ecabae4159b06ce13439249fe1d
Maurice Busch <gherrink@gmail.com>
2025-07-04 18:58:59 +0200

    feat(stub:elevate): init

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,11 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', [
-      'docs',
-      'pages',
-      'stub:base',
-      'stub:accordion',
-    ]],
+    'scope-enum': [2, 'always', ['docs', 'pages', 'stub:base', 'stub:accordion', 'stub:elevate']],
   },
 }
